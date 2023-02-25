eec87db6206d86d9069a871268ad4814d215984a
Pavel <55947748+PavelYakovlev1984@users.noreply.github.com>
2023-02-25 15:56:36 +0300

    1

--- src/js/_components.js
+++ src/js/_components.js
@@ -1,83 +1,83 @@
-console.log('components');
+// console.log('components');
 
-const game = {
-  team1: 'REAL MADRID',
-  team2: 'BARCELONA',
-  players: [
-    [
-    'Courtois',
-    'Vazquez',
-    'Militao',
-    'Nacho',
-    'Mendy',
-    'Casemiro',
-    'Valverde',
-    'Modrich',
-    'Kroos',
-    'Vinicius',
-    'Benzema',
-  ],
-  [
-    'Stegen',
-    'Minqueza',
-    'Araujo',
-    'Lenglet',
-    'Dest',
-    'Busquets',
-    'Jong',
-    'Alba',
-    'Messi',
-    'Pedri',
-    'Dembele',
-  ],
-],
-score: '2:1',
-scored: ['Kroos', 'Benzema', 'Mingueza'],
-date: 'Apr 10th. 2021',
-odds: {
-  team1: 1.48,
-  draw: 2.53,
-  team2: 4.25,
-}
+// const game = {
+//   team1: 'REAL MADRID',
+//   team2: 'BARCELONA',
+//   players: [
+//     [
+//     'Courtois',
+//     'Vazquez',
+//     'Militao',
+//     'Nacho',
+//     'Mendy',
+//     'Casemiro',
+//     'Valverde',
+//     'Modrich',
+//     'Kroos',
+//     'Vinicius',
+//     'Benzema',
+//   ],
+//   [
+//     'Stegen',
+//     'Minqueza',
+//     'Araujo',
+//     'Lenglet',
+//     'Dest',
+//     'Busquets',
+//     'Jong',
+//     'Alba',
+//     'Messi',
+//     'Pedri',
+//     'Dembele',
+//   ],
+// ],
+// score: '2:1',
+// scored: ['Kroos', 'Benzema', 'Mingueza'],
+// date: 'Apr 10th. 2021',
+// odds: {
+//   team1: 1.48,
+//   draw: 2.53,
+//   team2: 4.25,
+// }
 
-};
-// Диструктуризация
+// };
+// // Диструктуризация
 
-const [players1, players2] = game.players;
-// console.log(players1, players2);
+// const [players1, players2] = game.players;
+// // console.log(players1, players2);
 
-//rest
+// //rest
 
-const [goalkeeper, ...fieldPlayers] = players1;
-// console.log(goalkeeper, fieldPlayers);
-const allPlayers = [...players1, ...players2];
-// console.log(allPlayers);
+// const [goalkeeper, ...fieldPlayers] = players1;
+// // console.log(goalkeeper, fieldPlayers);
+// const allPlayers = [...players1, ...players2];
+// // console.log(allPlayers);
 
-//4
-const players1Total = [...players1, 'Marcelo', 'Isco', 'Diaz', 'Odriozola'];
-console.log(players1Total);
+// //4
+// const players1Total = [...players1, 'Marcelo', 'Isco', 'Diaz', 'Odriozola'];
+// console.log(players1Total);
 
-//5
- const {
-  odds: { team1, draw, team2},
- }=game;
+// //5
+//  const {
+//   odds: { team1, draw, team2},
+//  }=game;
 
-  console.log(team1, draw, team2);
+//   console.log(team1, draw, team2);
 
 
   //6
-  const printGoals = function (...gamePlayers) {
-    console.log(`${gamePlayers.length} goals scored`);
-  };
+  // const printGoals = function (...gamePlayers) {
+  //   console.log(`${gamePlayers.length} goals scored`);
+  // };
 
-  printGoals('Mingueza', 'Messi', 'Modrich', 'Nacho');
+  // printGoals('Mingueza', 'Messi', 'Modrich', 'Nacho');
 
-  printGoals(...game.scored);
+  // printGoals(...game.scored);
 
   //7
 
 
-  team1 < team2 && console.log(`${game.team1} is more likely to win`);
+  // team1 < team2 && console.log(`${game.team1} is more likely to win`);
 
   // for(const x of game.players[0]) {
   //   console.log(x);
